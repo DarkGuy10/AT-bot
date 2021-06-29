6a8c62453682e6b660a3595874f350223d274968
DarkGuy10 <HarperBurn@protonmail.ch>
2021-06-29 16:32:52 +0530

    Rikka pog

--- commands/verify.js
+++ commands/verify.js
@@ -14,7 +14,7 @@ module.exports = {
                     return message.channel.send(`**${member.tag}** is already verified!`);
                 await member.roles.remove(guildConfigs.unverifiedRoleID);
                 await member.roles.add(guildConfigs.defaultRoleID);
-                message.channel.send(`**${member.user.tag}** was verified by **${message.author.tag}**!`);
+                message.channel.send(new MessageEmbed().setColor('YELLOW').setImage('https://animeraifu.files.wordpress.com/2014/12/brdurw_chu2byo_lite_05_360p-mkv_snapshot_07-01_2012-10-27_15-00-58.jpg').setAuthor(message.author.tag, member.user.displayAvatarURL()).setTimestamp().setDescription(`**${member.user.tag}** was verified by **${message.author.tag}**!`));
                 message.guild.channels.resolve(guildConfigs.logsChannelID).send(new MessageEmbed().setColor('YELLOW').setAuthor(message.author.tag, message.author.displayAvatarURL()).setTimestamp().setDescription(`**${member.user.tag}** was verified by **${message.author.tag}** at ${new Date().toUTCString()}`));
             });
     }
