708112b20820306a42c48cd0b9f9e45a39375012
DarkGuy10 <HarperBurn@protonmail.ch>
2021-07-02 06:11:51 +0530

    fixed typo

--- commands/verify.js
+++ commands/verify.js
@@ -11,7 +11,7 @@ module.exports = {
         message.guild.members.fetch(toVerifyID)
             .then(async member => {
                 if(!member.roles.cache.has(guildConfigs.unverifiedRoleID))
-                    return message.channel.send(`**${member.tag}** is already verified!`);
+                    return message.channel.send(`**${member}** is already verified!`);
                 await member.roles.remove(guildConfigs.unverifiedRoleID);
                 await member.roles.add(guildConfigs.defaultRoleID);
                 message.channel.send(new MessageEmbed().setColor('YELLOW').setImage('https://animeraifu.files.wordpress.com/2014/12/brdurw_chu2byo_lite_05_360p-mkv_snapshot_07-01_2012-10-27_15-00-58.jpg').setAuthor(message.author.tag, member.user.displayAvatarURL()).setTimestamp().setDescription(`**${member.user.tag}** was verified by **${message.author.tag}**!`));
