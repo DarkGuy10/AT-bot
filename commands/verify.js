6ef87cb52737a57f663d342e1b2c513a4ef69e6a
DarkGuy10 <HarperBurn@protonmail.ch>
2021-07-02 06:09:15 +0530

    fixed regex

--- commands/verify.js
+++ commands/verify.js
@@ -7,8 +7,7 @@ module.exports = {
         if(message.channel.id != guildConfigs.waitingRoomChannelID || !message.member.roles.cache.has(guildConfigs.verifierRoleID))
             return;
 
-        //const toVerifyID =  args.join(' ').match(/<@![0-9]{18}>/)[0].slice(3, 21);
-        const toVerifyID = message.mentions.first.id;
+        const toVerifyID =  args.join(' ').match(/<@!?[0-9]{18}>/)[0].slice(3, 21);
         message.guild.members.fetch(toVerifyID)
             .then(async member => {
                 if(!member.roles.cache.has(guildConfigs.unverifiedRoleID))
